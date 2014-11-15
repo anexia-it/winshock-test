69e076c3981c20212880917377fb1eec5151fb75
Stephan Peijnik <speijnik@anexia-it.com>
2014-11-15 10:36:02 +0100

    Updated script logic a little bit in order to try to detect Windows Server 2012 R2.
    Signed-off-by: Stephan Peijnik <speijnik@anexia-it.com>

--- winshock_test.sh
+++ winshock_test.sh
@@ -33,7 +33,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-
+VERSION=0.2.0
 HOST=$1
 PORT=${2:-443}
 
@@ -44,8 +44,60 @@ then
   exit 1
 fi
 
-SERVER=$HOST:$PORT
-echo "Testing ${SERVER} for availability of SSL ciphers added in MS14-066..."
+echo "Checking if script is up-to-date..."
+REMOTE_VERSION=$(curl -k https://raw.githubusercontent.com/anexia-it/winshock-test/master/winshock_test.sh 2>/dev/null | grep '^VERSION=' | sed -e 's/^VERSION=//g')
+
+if [[ "$REMOTE_VERSION" != "$VERSION" ]]
+then
+  echo -e "\033[91mYou are running an outdated version of this script."
+  echo "The most recent version is $REMOTE_VERSION."
+  echo -e "It is highly recommended to update your script first.\033[0m"
+  read -p "Do you want to continue? (y/N) " -n 1 -r
+  if [[ ! "$REPLY" =~ ^[Yy]$ ]]
+  then
+    exit 2
+  fi
+else
+  echo "Script is up-to-date."
+fi
+
+echo -e "\n\033[91m"
+cat <<IMP
+*** IMPORTANT ***
+This script is intended to give you a hint on whether the MS14-66 patches
+have been installed or not.
+
+Please do NOT rely on the results this script is giving, as the correctness
+of the results can be impacted by manual modifications of cipher suites
+with tools like IIS Crypto or load balancers or SSL-offloaders between
+you and the target host.
+
+Also, this script is unreliable if the target system is running
+Windows Server 2012 R2, as the ciphers this script is testing for were present 
+on Windows Server 2012 R2 without the MS14-066 updates as well.
+If the checks are executed against IIS the result for Windows Server 2012 R2 
+will be presented as "UNKNOWN".
+IMP
+
+echo -e "\033[93m"
+
+cat <<WARN
+*** WARNING ***
+A negative result presented by this script does NOT mean that you do not
+have to install the MS14-66 patches on the target system. 
+Make sure to update all your Windows installations, regardless of the
+results this script gives you.
+
+WARN
+
+echo -e "\033[0m"
+read -p "I have read and understood the messages above. (y/N) " -n 1 -r
+if [[ ! "$REPLY" =~ ^[yY]$ ]]
+then
+  echo -e "\n\033[91mAborting. Please re-run and confirm that you have read and understood the messages above.\033[0m"
+  exit 4
+fi
+echo ""
 
 # According to https://technet.microsoft.com/library/security/ms14-066 the
 # following ciphers were added with the patch:
@@ -56,6 +108,28 @@ echo "Testing ${SERVER} for availability of SSL ciphers added in MS14-066..."
 #
 # The OpenSSL cipher names for these ciphers are:
 MS14_066_CIPHERS="DHE-RSA-AES256-GCM-SHA384 DHE-RSA-AES128-GCM-SHA256 AES256-GCM-SHA384 AES128-GCM-SHA256"
+# Ciphers supported by Windows Server 2012R2
+WINDOWS_SERVER_2012R2_CIPHERS="ECDHE-RSA-AES256-SHA384 ECDHE-RSA-AES256-SHA"
+
+# Test if OpenSSL does support the ciphers we're checking for...
+echo -n "Testing if OpenSSL supports the ciphers we are checking for: "
+openssl_ciphers=$(openssl ciphers)
+
+for c in $MS14_066_CIPHERS
+do
+  if ! echo $openssl_ciphers | grep -q $c 2>&1 >/dev/null
+  then
+    echo -e "\033[91mNO (OpenSSL does not support $c cipher.)\033[0m"
+    echo -e "\033[91mAborting."
+    exit 5
+  fi
+done
+
+echo -e "\033[92mYES\033[0m"
+
+SERVER=$HOST:$PORT
+
+echo -e "\n\033[94mTesting ${SERVER} for availability of SSL ciphers added in MS14-066...\033[0m"
 
 patched="no"
 for cipher in ${MS14_066_CIPHERS}
@@ -67,25 +141,27 @@ do
     err=$(echo $result | grep ^connect: \
       | sed -e 's/connect:errno=.*//g' -e 's/connect: //g')
     echo -e "\033[93mConnection error: $err"
-    echo -e "Aborting checks.\033[39m"
+    echo -e "Aborting checks.\033[0m"
     exit 1
   elif [[ "$result" =~ "SSL23_GET_SERVER_HELLO:unknown protocol" ]]
   then
     echo -e "\033[93mNo SSL/TLS support on target port."
-    echo -e "Aborting checks.\033[39m"
+    echo -e "Aborting checks.\033[0m"
     exit 1
   elif [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    : ${cipher}" ]]
   then
-    echo -e "\033[92mPASS\033[39m"
+    echo -e "\033[92mSUPPORTED\033[0m"
     if [[ "$patched" == "no" ]]
     then
       patched="yes"
     fi
   else
-    echo -e "\033[91mFAIL\033[39m"
+    echo -e "\033[91mUNSUPPORTED\033[0m"
   fi
 done
 
+windows_server_2012_or_later="no"
+windows_server_2012_r2="no"
 # added by @stoep: check whether a 443 port runs IIS
 if [[ "$PORT" == "443" ]]
 then
@@ -93,21 +169,94 @@ then
   echo -n "Testing if IIS is running on port 443: "
   if [[ $iis == *Microsoft-IIS* ]]
   then 
-    echo -e "\033[92mYES\033[39m"
-
+    iis_version=$(echo $iis | sed -e 's|Server: Microsoft-IIS/||g')
+    echo -e "\033[92mYES - Version ${iis_version}\033[0m"
+    if [[ $iis_version == *8.5* ]]
+    then
+      echo -e "\033[91mWindows Server 2012 R2 detected. Results of this script will be inconclusive.\033[0m"
+      windows_server_2012_or_later="yes"
+      windows_server_2012_r2="yes"
+    elif [[ $iis_version == *8.0* ]]
+    then
+      windows_server_2012_or_later="yes"
+      windows_server_2012_r2="no"
+    fi
   else
-    echo -e "\033[91mNO\033[39m"
-    echo -e "\033[93mNOTE:\nResults may be incorrect if HTTPS server is not using SChannel or has a customized cipher list.\033[39m"
+    echo -e "\033[91mNO\033[0m"
   fi
 fi
 
-if [[ "$patched" == "yes" ]]
+# Check if Windows Server 2012 or later is running on the remote system...
+if [[ "$windows_server_2012_or_later" == "no"  && "$PORT" != "443" ]]
 then
-  patched="\033[92mYES\033[39m"
-else
-  patched="\033[91mNO\033[39m"
+  echo -e "\033[94mChecking if target system is running Windows Server 2012 or later...\033[0m"
+  for cipher in ${WINDOWS_SERVER_2012R2_CIPHERS}
+  do
+    echo -en "Testing cipher ${cipher}: "
+    result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER 2>&1)
+    if [[ "$result" =~ "connect:errno=" ]]
+    then
+      err=$(echo $result | grep ^connect: \
+        | sed -e 's/connect:errno=.*//g' -e 's/connect: //g')
+      echo -e "\033[93mConnection error: $err"
+      echo -e "Aborting checks.\033[0m"
+      exit 1
+    elif [[ "$result" =~ "SSL23_GET_SERVER_HELLO:unknown protocol" ]]
+    then
+      echo -e "\033[93mNo SSL/TLS support on target port."
+      echo -e "Aborting checks.\033[0m"
+      exit 1
+    elif [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    : ${cipher}" ]]
+    then
+      echo -e "\033[92mSUPPORTED\033[0m"
+      if [[ "$windows_server_2012_or_later" == "no" ]]
+      then
+        windows_server_2012_or_later="yes"
+        break
+      fi
+    else
+      echo -e "\033[91mUNSUPPORTED\033[0m"
+    fi
+  done
 fi
 
-echo -e "System at $SERVER seems to be patched: $patched"
-echo -e "\033[93mNOTE:\nThe information above may be incorrect if the available SSL ciphers have been\nmodified manually or you are not directly connecting to the target system.\033[39m"
+if [[ "$patched" == "yes" && "$windows_server_2012_or_later" == "no" ]]
+then
+  patched="\033[92mYES\033[0m"
+elif [[ "$patched" == "yes" ]]
+then
+  patched="\033[93mUNKNOWN"
+  if [[ "$windows_server_2012_r2" == "yes" ]]
+  then
+    patched="$patched: Windows Server 2012 R2 detected."
+  else
+    patched="$patched: Windows Server 2012 or later detected."
+  fi
+else
+  patched="\033[91mNO\033[0m"
+fi
+
+echo -e "\033[94m$SERVER is patched: $patched\033[0m"
+echo -e "\n\033[93m"
+cat <<EOF 
+*** IMPORTANT ***
+
+Please keep in mind that the patch-status reported above is only a hint and
+may generate both false-positive and false-negative results in some cases.
+
+If Windows Server 2012 R2 is reported above results WIIL BE incorrect.
+If Windows Server 2012 or later is reported above results MAY BE incorrect,
+please test again against IIS running on port 443.
+
+The information above may be incorrect if:
+
+* the available SSL ciphers have been modified manually
+* you are not directly connecting to the target system
+* the target system is running Windows Server 2012 R2
+
+Please do apply the MS14-066 patches to all your systems regardless
+of the results presented above!
+
+EOF
+echo -en "\033[0m"
 exit 0
